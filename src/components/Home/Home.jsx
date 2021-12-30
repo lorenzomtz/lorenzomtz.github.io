98e0ce95fb6e8160749e4951e57173ec804b388f
Lorenzo Martinez <lorenzomtz@utexas.edu>
2021-12-30 00:25:36 -0600

    Working on projects, creating cnm info

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -38,7 +38,7 @@ function Home() {
             </p>
           </div>
         </div>
-        <div id="about">
+        <div className="section" id="about">
           <div className="title">
             <h2>About Me</h2>
           </div>
@@ -124,7 +124,7 @@ function Home() {
             </div>
           </div>
         </div>
-        <div id="experience">
+        <div className="section" id="experience">
           <div className="title">
             <h2>Experience</h2>
           </div>
@@ -186,6 +186,158 @@ function Home() {
               <li>
                 Met with mentor on weekly basis to provide detailed project
                 reports and milestone updates.
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="section" id="projects">
+          <div className="title">
+            <h2>Projects</h2>
+          </div>
+          <div className="info">
+            <h4>Chemicals Near Me</h4>
+            <h5>React/JavaScript (ES6+), HTML/CSS, Python</h5>
+            <ul>
+              <li>
+                Hosted a dynamic website through{" "}
+                <a
+                  href="https://aws.amazon.com/amplify/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  AWS Amplify
+                </a>{" "}
+                after obtaining a domain name through{" "}
+                <a
+                  href="https://www.namecheap.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  NameCheap
+                </a>
+                .
+              </li>
+              <li>
+                Scraped data from multiple different APIs and formatted into
+                suitable{" "}
+                <a
+                  href="https://www.json.org/json-en.html"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  JSON
+                </a>{" "}
+                files using{" "}
+                <a
+                  href="https://www.python.org/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Python
+                </a>
+                .
+              </li>
+              <li>
+                Exported data into a database hosted on{" "}
+                <a
+                  href="https://aws.amazon.com/rds/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Amazon RDS
+                </a>{" "}
+                via{" "}
+                <a
+                  href="https://www.postgresql.org/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  PostgreSQL
+                </a>{" "}
+                utilizing{" "}
+                <a
+                  href="https://flask-sqlalchemy.palletsprojects.com/en/2.x/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Flask SQLAlchemy
+                </a>
+                .
+              </li>
+            </ul>
+          </div>
+          <div className="info">
+            <h4>Searchvis</h4>
+            <h5>Python</h5>
+            <ul>
+              <li>
+                Implemented a{" "}
+                <a
+                  href="https://en.wikipedia.org/wiki/Graphical_user_interface"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  GUI
+                </a>{" "}
+                for visualizing various common pathfinding algorithms.
+              </li>
+              <li>
+                Provided functions for users to create obstacles for
+                pathfinding.
+              </li>
+              <li>
+                Researched common search algorithms (
+                <a
+                  href="https://en.wikipedia.org/wiki/Breadth-first_search"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  BFS
+                </a>, {" "}
+                <a
+                  href="https://en.wikipedia.org/wiki/Depth-first_search"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  DFS
+                </a>, {" "}
+                <a
+                  href="https://www.geeksforgeeks.org/uniform-cost-search-dijkstra-for-large-graphs/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  UCS
+                </a>,
+                and {" "}
+                <a
+                  href="https://en.wikipedia.org/wiki/A*_search_algorithm"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  A*
+                </a>) and how to implement them for different uses.
+              </li>
+            </ul>
+          </div>
+          <div className="info">
+            <h4>PintOS Projects</h4>
+            <h5>C/x86</h5>
+            <ul>
+              <li>
+                Added priority scheduling with timer interrupts for ordering
+                processes in the CPU.
+              </li>
+              <li>
+                Implemented argument passing for execution of user programs.
+              </li>
+              <li>Created system calls for user programs to readily use.</li>
+              <li>
+                Expanded the virtual memory system, adding a page replacement
+                system and the use of swap.
+              </li>
+              <li>
+                Expanded the existing file system to allow access by multiple
+                threads and support multi-level indexed files.
               </li>
             </ul>
           </div>
